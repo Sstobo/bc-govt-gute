8127deba789bd4ccfee2141a526275e98b5d1338
Sstobo <seanstobo@gmail.com>
2023-10-25 22:26:02 -0700

    adding exclusions

--- zipmypackage.sh
+++ zipmypackage.sh
@@ -11,6 +11,13 @@ OUTPUT_FILE="./$PACKAGE_NAME.zip"
 
 # Exclude the node_modules directory
 EXCLUDE="node_modules/*"
+EXCLUDE="public/*"
+EXCLUDE=".gitignore"
+EXCLUDE="package-lock.json"
+EXCLUDE="package.json"
+EXCLUDE="README.md"
+EXCLUDE="zipmypackage.sh"
+
 
 # This will zip the directory excluding the node_modules
 zip -r "$OUTPUT_FILE" "$DIR_TO_ZIP" -x "$EXCLUDE"
